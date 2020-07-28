9398ae83cea30b480e77853acdcc0b654955f7fe
Michael-C-Nurse <michael.c.nurse@gmail.com>
2020-07-28 11:14:53 +0200

    py linked list sum

---
+++ add_two_linked_lists.py
@@ -0,0 +1,67 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single
+digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, 
+except the number 0 itself.
+
+My note: the lists can be any length - and the class cannot be iterated, so
+you will have to use functional programming to re-call itself repeatedly.
+
+
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+'''
+
+class Solution:
+    def addTwoNumbers(self, linked_list1: ListNode, linked_list2: ListNode, carry=0) -> 'ListNode':
+
+        """   
+        Take two linked lists (node lists) and
+        using functional programming add the
+        elements together to return the sum.
+
+        :rtype: ListNode
+        """
+        
+        #bring any previous carries forward fromt the 
+        #previous node and add it to the current number
+        current_node_value = linked_list1.val + linked_list2.val + carry 
+        #print(current_node_value)
+        
+        #update the carry for the current node
+        #use double slash to drop any remainder
+        carry = current_node_value // 10
+        #print(carry)
+        
+        #Build up the first answer digit in a linked list.
+        answer = ListNode(current_node_value % 10)
+        #print(answer)
+        
+        #Use logic to see if the method must call itself.
+        #Use logic to populate null fields in the original
+        #linked lists with zeros. e.g. for adding 1000 and 3.
+        
+        if (linked_list1.next != None or linked_list2.next != None or carry != 0):
+           
+          if linked_list1.next == None:
+              linked_list1.next = ListNode(0)
+          if linked_list2.next == None:
+              linked_list2.next = ListNode(0)
+          
+          answer.next = self.addTwoNumbers(linked_list1.next, linked_list2.next, carry)
+        
+        return answer   
+
+'''
+Leetcode result
+
+Success
+
+Runtime: 76 ms, faster than 66.98% of Python3 online submissions for Add Two Numbers.
+'''
+        
